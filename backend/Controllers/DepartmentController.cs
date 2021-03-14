fc1ed66c5cb8bbac277880b28c0d87b93d9584a3
Junfeng ou <junfengou@gmail.com>
2021-03-14 17:08:02 -0500

    Just a short little employee handbook app built with react and dotnet

---
+++ backend/Controllers/DepartmentController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApiEmployee.Models;
+
+namespace WebApiEmployee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }  
+        
+        [HttpGet]
+        public JsonResult Get() // To work with Database, we need a Nuget package called...System.Data.SqlClient
+        {
+            string query = @"select DepartmentId, DepartmentName from dbo.Department"; // Avoid this....use SP or Entity Framework
+            DataTable table = new DataTable(); // creating an empty table
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource)) // Accessing the database with the passed in connection string
+            {
+                myCon.Open(); // open the connection
+                using(SqlCommand myCommand = new SqlCommand(query, myCon)) // pass in the query command and the connection string
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close(); // close off the reader
+                    myCon.Close(); // close off the connection
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string insert = @"
+                    insert into dbo.Department values 
+                    ('" + dep.DepartmentName + @"')
+                    ";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(insert, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string update = @"
+                    update dbo.Department set 
+                    DepartmentName = '" + dep.DepartmentName + @"'
+                    where DepartmentId = " + dep.DepartmentId + @" 
+                    ";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(update, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string delete = @"
+                    DELETE FROM Department
+                    WHERE DepartmentId = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(delete, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
